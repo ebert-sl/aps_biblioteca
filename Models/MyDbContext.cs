bda9ed3fbc8db3e9ef3345be37b2f7baf49d29aa
ebert-sl <ebertis2002@gmail.com>
2023-10-30 21:20:43 -0300

    primeiro commit

---
+++ Models/MyDbContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace biblioteca.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) 
+        {
+        }
+
+    }
+}
